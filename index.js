91547692b92543cc9c2e2aac133802e2bafef798
itsphoebee <phoebelau7@gmail.com>
2017-09-11 04:05:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ if (name === "Susan") {
 else {
   console.log("Expected the name to be Susan")
 }
+if (height === 74) {
+  console.log ("The height is correct")
+}
+else {
+  console.log("Expected height of 74")
+}
+
 // Don't worry about this
 module.exports = { name, height
 }
